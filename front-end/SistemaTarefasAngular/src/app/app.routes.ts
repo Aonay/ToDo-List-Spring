b27901933c93b4cc25abc5217a90363629d1a6b6
Vitor Marvulle <marvullevitor@gmail.com>
2024-12-03 18:39:14 -0300

    route guard + btn excluir

--- front-end/SistemaTarefasAngular/src/app/app.routes.ts
+++ front-end/SistemaTarefasAngular/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/pages/home/home.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { FormTarefaComponent } from './components/forms/form-tarefa/form-tarefa.component';
+import { AuthGuard } from './components/pages/dashboard/modal/guards/auth.guard.spec';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, //redireciona sempre para /home
@@ -12,7 +13,7 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent}, 
     { path: 'login', component: FormLoginComponent }, 
     { path: 'cadastro', component: FormCadastroComponent }, 
-    { path: 'dashboard', component: DashboardComponent }, 
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, // Protege a rota com o guard
     { path: 'tarefas', component: FormTarefaComponent } 
   ];
 
