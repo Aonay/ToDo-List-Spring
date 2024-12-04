06bcbff9f8c764cba23f0ed23b59d4ee734139af
AlanMatias <alan.rodrigues.m@gmail.com>
2024-12-04 01:17:52 -0300

    Descricao na Documentacao

--- back-end/src/main/java/br/edu/fatecpg/tarefa/Sistema_Tarefas_SPRING/controller/UsuarioController.java
+++ back-end/src/main/java/br/edu/fatecpg/tarefa/Sistema_Tarefas_SPRING/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.edu.fatecpg.tarefa.Sistema_Tarefas_SPRING.controller;
 import br.edu.fatecpg.tarefa.Sistema_Tarefas_SPRING.model.Usuario;
 import br.edu.fatecpg.tarefa.Sistema_Tarefas_SPRING.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(summary = "Cadastra um usuario")
     @PostMapping("/registrar")
     public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario user) {
         Usuario usuario = usuarioService.registrar(user);
         return ResponseEntity.ok(usuario);
     }
 
+    @Operation(summary = "Faz o login no sistema")
     @PostMapping("/login")
     public ResponseEntity<Usuario> logarUsuario(@RequestBody Usuario login, HttpSession session) {
         Usuario usuarioLogado = usuarioService.login(login);
@@ -27,6 +30,7 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioLogado);
     }
 
+    @Operation(summary = "Ecenrra a secao ativa")
     @PostMapping("/logout")
     public ResponseEntity<String> deslogarUsuario(HttpSession session) {
         session.invalidate();
